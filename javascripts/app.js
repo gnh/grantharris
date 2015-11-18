4b3e469645531350a384ba7c7c38fe66a35503b5
Grant Harris <me@grantharr.is>
2015-11-18 01:58:04 +0000

    logo hover state under fold

--- javascripts/app.js
+++ javascripts/app.js
@@ -5,13 +5,30 @@ $(window).scroll(function(){
   if(scrollTop >= 308){
     $('#logo').css('width', '2.5rem')
               .css('height', '2.5rem')
-              .css('fill', '#d2d3e3');
+              .css('fill', '#74A7FF')
+              .css('transform', 'rotate(-90deg)');
   }
   else{
     $('#logo').css('width', '5rem')
               .css('height', '5rem')
-              .css('fill', '#24252B');
+              .css('fill', '#fff')
+              .css('transform', 'rotate(0deg)');
   }
 
+});
+
+
+$('#logo').mouseenter(function(){
+    $('#logo').css('width', '5rem')
+              .css('height', '5rem')
+              .css('fill', '#fff')
+              .css('transform', 'rotate(0deg)');
+});
+
+$('#logo').mouseleave(function(){
+    $('#logo').css('width', '2.5rem')
+              .css('height', '2.5rem')
+              .css('fill', '#74A7FF')
+              .css('transform', 'rotate(-90deg)');
 });
 
