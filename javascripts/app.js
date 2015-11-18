bae677960aaf273cc4f1516decf6a138613b8b33
Grant Harris <me@grantharr.is>
2015-11-18 01:23:42 +0000

    logo + animation beta

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,27 +1,17 @@
-// Grant Harris - 2015v2.0
-
 $(window).scroll(function(){
 
-  // Scroll & Window variables
   var scrollTop = $(window).scrollTop();
-  var windowHeight = $(window).height();
-  var fadeProgress = 1 - (scrollTop / (.5 * windowHeight));    // Opacity: 0; after scroll down 50% of viewport height
-  var slideProgress = 1 - (windowHeight * (scrollTop / windowHeight));  // Return desired percentage of distance scrolled
 
-  // negative scrolling zero
-  if (scrollTop < 0) scrollTop = 0;
-
-  // var pct = scrollTop / windowHeight;
-  // if(pct > 1) pct = 1;
-
-  if (slideProgress > 1) slideProgress = 1;
-
-  // Fade & offset position during scroll event (via xProgress variables)
-  $('section > .wrap').css('opacity', fadeProgress);
-  $('section > .wrap').css('transform', 'translateY(' +  -0.4 * slideProgress + 'px)');
+  if(scrollTop >= 308){
+    $('#logo').css('width', '2.5rem')
+              .css('height', '2.5rem')
+              .css('fill', '#d2d3e3');
+  }
+  else{
+    $('#logo').css('width', '5rem')
+              .css('height', '5rem')
+              .css('fill', '#24252B');
+  }
 
 });
-
-
-// prevent elastic scrolling
 
