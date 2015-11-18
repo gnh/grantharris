5439bbc807b2f82ea8627642930cd6a8ba35ca2e
Grant Harris <me@grantharr.is>
2015-11-18 02:27:13 +0000

    fixed hover state working above fold

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,32 +3,24 @@ $(window).scroll(function(){
   var scrollTop = $(window).scrollTop();
 
   if(scrollTop >= 308){
-    $('#logo').css('width', '2.5rem')
-              .css('height', '2.5rem')
-              .css('fill', '#74A7FF')
-              // .css('transform', 'rotate(-90deg)');
+    $('#logo').css('width', '2.5rem').css('height', '2.5rem').css('fill', '#74A7FF');
+
+    $('#logo').mouseenter(function(){
+      $('#logo').css('width', '5rem').css('height', '5rem').css('fill', '#fff');
+    });
+
+    $('#logo').mouseleave(function(){
+        $('#logo').css('width', '2.5rem').css('height', '2.5rem').css('fill', '#74A7FF');
+    });
   }
+  
   else{
-    $('#logo').css('width', '5rem')
-              .css('height', '5rem')
-              .css('fill', '#fff')
-              // .css('transform', 'rotate(0deg)');
+    $('#logo').css('width', '5rem').css('height', '5rem').css('fill', '#fff');
+
+    $('#logo').unbind('mouseenter');
+
+    $('#logo').unbind('mouseleave');
   }
-
-});
-
-
-$('#logo').mouseenter(function(){
-    $('#logo').css('width', '5rem')
-              .css('height', '5rem')
-              .css('fill', '#fff')
-              // .css('transform', 'rotate(0deg)');
-});
-
-$('#logo').mouseleave(function(){
-    $('#logo').css('width', '2.5rem')
-              .css('height', '2.5rem')
-              .css('fill', '#74A7FF')
-              // .css('transform', 'rotate(-90deg)');
+  
 });
 
